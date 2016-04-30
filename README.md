94c58c58e10526958394a7ab9c96bafb26108ddc
vjdhama <vjdhama26@gmail.com>
2016-05-01 02:41:22 +0530

    updated kemal to latest version for example

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Add this to your application's `shard.yml`:
 dependencies:
   ambience:
     github: vjdhama/ambience
-    version: ~> 0.3.9
+    version: ~> 0.4.0
 ```
 
 ## Usage
--- examples/kemal/your_app/src/your_app.cr
+++ examples/kemal/your_app/src/your_app.cr
@@ -1,7 +1,10 @@
 require "kemal"
-require "ambience"
+require "../../../../src/ambience"
 
 Ambience.application(File.expand_path("#{__DIR__}/../config/configuration.yml"), "development")
 Ambience.load
 
 require "./your_app/*"
+
+Kemal.run
+
